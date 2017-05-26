46a2faa579f1734a1b758a606a0554ab137d4358
MihaelaGrubii <mihaela.grubii@mail.ru>
2017-05-26 03:28:20 +0300

    source codul

--- sprite.js
+++ sprite.js
@@ -1,5 +1,7 @@
 var 
 
+// Sprite vars //
+
 s_bird,
 s_bg,
 s_fg,
@@ -12,7 +14,15 @@ s_buttons,
 s_numberS,
 s_numberB;
 
-
+/**
+ * Simple sprite class
+ * 
+ * @param {Image}  img    spritesheet image
+ * @param {number} x      x-position in spritesheet
+ * @param {number} y      y-position in spritesheet
+ * @param {number} width  width of sprite 
+ * @param {number} height height of sprite
+ */
 function Sprite(img, x, y, width, height) {
 	this.img = img;
 	this.x = x*2;
@@ -20,69 +30,86 @@ function Sprite(img, x, y, width, height) {
 	this.width = width*2;
 	this.height = height*2;
 };
+/**
+ * Draw sprite ta canvas context
+ * 
+ * @param  {CanvasRenderingContext2D} ctx context used for drawing
+ * @param  {number} x   x-position on canvas to draw from
+ * @param  {number} y   y-position on canvas to draw from
+ */
 Sprite.prototype.draw = function(ctx, x, y) {
 	ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
 		x, y, this.width, this.height);
 };
 
+/**
+ * Initate all sprite
+ * 
+ * @param  {Image} img spritesheet image
+ */
 function initSprites(img) {
 
 	s_bird = [
-		new Sprite(img, 350, 0, 30, 30),
-		new Sprite(img, 350, 30, 30, 30),
-		new Sprite(img, 350, 60, 30, 30),
-		new Sprite(img, 355, 90, 28, 28),
-		new Sprite(img, 350, 120, 30, 25),
-		new Sprite(img, 350, 145, 30, 30),
-		new Sprite(img, 350, 175, 30, 20),
-		new Sprite(img, 350, 195, 30, 30),
-		new Sprite(img, 350, 225, 30, 30),
-		new Sprite(img, 385, 0, 35, 35),
-		new Sprite(img, 385, 35, 35, 40),
-		new Sprite(img, 385, 75, 35, 40),
-		new Sprite(img, 385, 115, 35, 35),
-		new Sprite(img, 385, 150, 35, 35),
-		new Sprite(img, 385, 185, 35, 35),
-		new Sprite(img, 385, 225, 35, 30),
-		new Sprite(img, 418, 0, 25, 40),
-		new Sprite(img, 418, 40, 25, 40),
-		new Sprite(img, 418, 80, 25, 25),
-		new Sprite(img, 418, 120, 25, 35),
-		new Sprite(img, 418, 155, 25, 30),
-		new Sprite(img, 418, 185, 25, 40),
-		new Sprite(img, 418, 225, 25, 25)
+		new Sprite(img, 156, 115, 17, 12),
+		new Sprite(img, 156, 128, 17, 12),
+		new Sprite(img, 156, 141, 17, 12),
+		new Sprite(img, 175, 115, 17, 13),
+		new Sprite(img, 175, 129, 17, 16),
+		new Sprite(img, 175, 144, 17, 16),
+		new Sprite(img, 192, 115, 17, 12),
+		new Sprite(img, 192, 128, 17, 12),
+		new Sprite(img, 192, 142, 17, 12)
 	];
 	
-	s_bg = new Sprite(img,   0, 0, 242, 143);
-	s_bg.color = "#70C5CF";
-	s_fg = new Sprite(img, 0, 113, 242, 30);
+	s_bg = new Sprite(img,   0, 0, 138, 114);
+	s_bg.color = "#C1A8D0"; // save background color
+	s_fg = new Sprite(img, 138, 0, 112,  56);
 	
-	s_pipeNorth = new Sprite(img, 270, 0, 80, 120);
-	s_pipeSouth = new Sprite(img, 270, 122, 80, 130);
+	s_pipeNorth = new Sprite(img, 252, 75, 26, 130);
+	s_pipeSouth = new Sprite(img, 277, 0, 26, 117);
 	
 	s_text = {
-		Flopps: new Sprite(img, 120, 145, 150, 33),
-		GameOver:   new Sprite(img, 120, 180, 95, 20),
-		GetReady:   new Sprite(img, 120, 155, 90, 25)
+		FlappyBird: new Sprite(img, 59, 114, 96, 22),
+		GameOver:   new Sprite(img, 59, 136, 94, 19),
+		GetReady:   new Sprite(img, 59, 155, 87, 22)
 	}
 	s_buttons = {
-		Rate:  new Sprite(img,  224, 242, 40, 14),
-		Menu:  new Sprite(img, 144, 228, 40, 14),
-		Share: new Sprite(img, 184, 228, 40, 14),
-		Score: new Sprite(img, 184, 242, 40, 14),
-		Ok:    new Sprite(img, 224, 228, 40, 14),
-		Start: new Sprite(img, 144, 242, 40, 14)
+		Rate:  new Sprite(img,  79, 177, 40, 14),
+		Menu:  new Sprite(img, 119, 177, 40, 14),
+		Share: new Sprite(img, 159, 177, 40, 14),
+		Score: new Sprite(img,  79, 191, 40, 14),
+		Ok:    new Sprite(img, 119, 191, 40, 14),
+		Start: new Sprite(img, 159, 191, 40, 14)
 	}
 
-	s_score  = new Sprite(img, 3,  144, 115, 58);
-	s_splash = new Sprite(img,   215, 180,  40, 25);
+	s_score  = new Sprite(img, 138,  56, 113, 58);
+	s_splash = new Sprite(img,   0, 114,  59, 49);
 
-	s_numberS = new Sprite(img, 5, 215, 90,  10);
-	s_numberB = new Sprite(img, 5, 215, 90, 10);
+	s_numberS = new Sprite(img, 0, 177, 6,  7);
+	s_numberB = new Sprite(img, 0, 188, 7, 10);
 
-	s_numberS.draw = s_numberB.draw = function(ctx, x, y, num) {
+	/**
+	 * Draw number to canvas
+	 * 
+	 * @param  {CanvasRenderingContext2D} ctx context used for drawing
+	 * @param  {number} x      x-position
+	 * @param  {number} y      y-position
+	 * @param  {number} num    number to draw
+	 * @param  {number} center center to offset from
+	 * @param  {number} offset padd text to draw right to left
+	 */
+	s_numberS.draw = s_numberB.draw = function(ctx, x, y, num, center, offset) {
 		num = num.toString();
+
 		var step = this.width + 2;
+		
+		if (center) {
+			x = center - (num.length*step-2)/2;
+		}
+		if (offset) {
+			x += step*(offset - num.length);
+		}
+
 		for (var i = 0, len = num.length; i < len; i++) {
 			var n = parseInt(num[i]);
 			ctx.drawImage(img, step*n, this.y, this.width, this.height,
